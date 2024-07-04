132ec091e917606df9631d43e08b3e2842ed0c2e
Nirbhay <nirbhaypaliwal05@gmail.com>
2024-07-04 18:02:40 +0530

    Navbar Added

---
+++ my-app/src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+import Herosection from "./components/Herosection";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.90] antialised bg-grid-white/[0.02]">
+      <Herosection/>
+      <Footer/>
+    </main>
+  );
+}
